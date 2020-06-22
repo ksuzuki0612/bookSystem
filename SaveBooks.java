8e7be7d96dda7fc9826310d9c4546898316ccf2f
jojoRohan <66408935+jojoRohan@users.noreply.github.com>
2020-06-22 17:20:37 +0900

    UserMenuを2 partsに分けました

---
+++ SaveBooks.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SaveBooks extends HttpServlet{
+    Logger logger = Logger.getLogger(SaveBooks.class.getName());
+    List<Book> titleList = new ArrayList<>();
+    final int errorNum = 0;
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException{
+        logger.entering(LogUtil.getClassName(), LogUtil.getMethodName());
+        String saveFile = request.getParameter("saveFileName");
+        try{
+            File csv = new File(saveFile);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(csv));
+            for(Book t : titleList){
+                bw.write(
+                    "ISBN,"+t.getISBN()+
+                    ",Title,"+ t.getTitle()+
+                    ",Publisher,"+t.getPublisher()+
+                    ",PublishDate,"+new SimpleDateFormat("yyyy/MM/dd").format(t.getPublishDate())+
+                    ",Authors,"+ t.getStringAuthors()+
+                    ",category, "+ t.getField()+
+                    ",Inventory,"+ t.getInventory()+
+                    ",BorrowedAmount," +t.getBorrowedAmount());
+                bw.newLine();
+            }
+            bw.close();
+        }catch(IOException e){
+            errorNum = 1;    //日付の型が正しくありません
+            e.printStackTrace();
+            logger.severe("SEVERE");
+        }
+        finally{
+            logger.exiting(LogUtil.getClassName(), LogUtil.getMethodName());
+        }
+    }
+}
