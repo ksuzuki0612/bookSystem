c0e746685bee2d112232e73ccf1f411d0632bbf8
Kazutaka Hiramatsu <kazutaka.hiramatsu@aimnext.com>
2020-06-24 13:51:02 +0900

    UserMenuNum.javaの削除取り消し

---
+++ UserMenuNum.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class UserMenuNum extends HttpServlet{
+    final static int SearchBook = 1;
+    final static int CancelReservation = 2;
+    final static int ReturnProgram = 3;
+}
