ad37c6abb6f65ecf4a69c36c87a4e6028fb637d9
kazutaka-hiramatsu <66239369+kazutaka-hiramatsu@users.noreply.github.com>
2020-06-24 11:22:16 +0900

    Delete UserMenuNum.java

--- UserMenuNum.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-public class UserMenuNum extends HttpServlet{
-    final static int SearchBook = 1;
-    final static int CancelReservation = 2;
-    final static int ReturnProgram = 3;
-}
